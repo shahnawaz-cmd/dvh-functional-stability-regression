551dbf6ccf62550d532f859d6e4982de1d647a2c
Shahnawaz <shahnawaz-cmd@users.noreply.github.com>
2026-09-10 00:03:57 +0500

    config: Set 2 test retries in playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -4,6 +4,7 @@ const { defineConfig } = require('@playwright/test');
 module.exports = defineConfig({
   testMatch: ['**/*.spec.js'],
   timeout: 120000,
+  retries: 2,
   reporter: [
     ['list'],
     ['html', { open: 'never' }],
